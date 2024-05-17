af9e5b22132001ec4487ab139dba80f35fe6c239
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2024-05-17 13:07:59 +0500

    Updates

---
+++ assets/js/about.js
@@ -0,0 +1,68 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+// import { gsap } from "gsap"
+// import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+document.addEventListener("DOMContentLoaded", () => {
+  // gsap.registerPlugin(ScrollTrigger)
+
+  const historySection = document.querySelector('.history-section')
+  const historySectionLine = document.querySelector('.history-section__line')
+  const historySectionInner = document.querySelector('.history-section__inner')
+  const histories = document.querySelector('.histories')
+  const historiesGradient = document.querySelector('.histories__gradient')
+
+  historySection.style.height = histories.scrollHeight + 'px'
+  histories.style.maxHeight = window.innerHeight + 'px'
+
+  const startY = historySection.offsetTop + historySectionLine.offsetTop
+  const y = histories.scrollHeight - historySectionInner.clientHeight
+
+  window.addEventListener("scroll", srollCallback);
+  srollCallback()
+  function srollCallback() {
+    const trX = window.scrollY - startY
+
+    if (trX > 0 && trX < y) {
+      histories.style.transform = `translateY(${trX}px)`
+      historiesGradient.style.transform = `translateY(${trX}px)`
+      histories.scrollTop = trX
+    } else if (trX > y) {
+      histories.style.transform = `translateY(${y}px)`
+      historiesGradient.style.transform = `translateY(${y}px)`
+      histories.scrollTop = y
+    }
+    else {
+      histories.style.transform = `translateY(${0}px)`
+      historiesGradient.style.transform = `translateY(${0}px)`
+      histories.scrollTop = 0
+    }
+  }
+})
+
+
+// gsap.to('.history-section__inner', {
+//   scrollTrigger: {
+//     trigger: '.history-section',
+//     start: "top top",
+//     end: "bottom " + bottomOffet + 'px',
+//     scrub: true,
+//     toggleActions: 'restart pause reverse pause',
+//   },
+//   y: y,
+// })
+
+// gsap.to('.histories', {
+//   scrollTrigger: {
+//     trigger: '.history-section',
+//     start: "top top",
+//     end: "bottom " + bottomOffet + 'px',
+//     scrub: true,
+//     toggleActions: 'restart pause reverse pause',
+//   },
+//   scrollTop: y,
+// })
+
+/******/ })()
+;
+//# sourceMappingURL=about.js.map
---
+++ assets/js/common.js
@@ -0,0 +1,74 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+document.addEventListener('DOMContentLoaded', () => {
+  customToggler()
+  customSelect()
+})
+
+const CUSTOME_TOGGLER = {
+  'modal': {
+    actavated() {
+      document.body.style.overflow = 'hidden'
+
+      const nav = document.body.querySelector('[data-target="header__burger"]')
+      if (nav) {
+        nav.classList.remove('header__burger--active')
+      }
+    },
+    deactivated() {
+      document.body.style.overflow = null
+    }
+  }
+}
+
+function customToggler() {
+  document.querySelectorAll('[data-trigger]')?.forEach((trigger) => {
+    trigger.addEventListener('click', () => {
+      document.querySelectorAll(`[data-target=${trigger.dataset.trigger}]`)?.forEach((target) => {
+        const addFunc = CUSTOME_TOGGLER[target.dataset.handler]
+        if (target.classList.contains(target.dataset.class)) {
+          if (addFunc) addFunc.deactivated()
+          target.classList.remove(target.dataset.class);
+        } else {
+          if (addFunc) addFunc.actavated()
+          target.classList.add(target.dataset.class);
+        }
+      })
+    })
+  })
+}
+
+function customSelect() {
+  const customSelects = document.querySelectorAll('.custom-select')
+
+  customSelects.forEach((customSelect) => {
+    const customSelectInput = customSelect.querySelector('.custom-select__input')
+    const input = customSelectInput.querySelector('input')
+
+    customSelectInput.addEventListener('click', () => {
+      customSelect.classList.toggle('custom-select--active')
+    })
+
+    customSelect.addEventListener('click', (e) => {
+      const isOption = e.target.closest('.custom-select__option')
+      if (isOption) {
+        const value = isOption.querySelector('input')?.value
+
+        if (value) {
+          input.value = value
+        }
+      }
+    })
+
+    document.addEventListener('click', (e) => {
+      const isSelect = e.target.closest('.custom-select')
+
+      if ((!isSelect || isSelect !== customSelect) && customSelect.classList.contains('custom-select--active')) {
+        customSelect.classList.remove('custom-select--active')
+      }
+    })
+  })
+}
+/******/ })()
+;
+//# sourceMappingURL=common.js.map
